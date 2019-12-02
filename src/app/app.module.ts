aa751ecf97a606b2583cee6ff7645da095e33cfd
elkingutierrex <Pro-egutierrez@intergrupo.com>
2019-12-02 10:46:00 -0500

    267 - Componentes de la aplicación YouTube

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
+//services
+import { YoutubeService } from "./services/youtube.service";
+
+
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { from } from 'rxjs';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    YoutubeService    
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
